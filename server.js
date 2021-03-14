bed831a678f91fdc109ae1c2e16b94aa49c5d5ee
The-Anton <shekhar59324@gmail.com>
2021-03-14 19:46:35 +0530

    changed endpoint format4

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // create an express app
+'use strict';
 const express = require("express")
 const request = require('request')
 //var firebase = require('firebase')
@@ -12,20 +13,9 @@ var revGeoCodingUrl2 = "https://us1.locationiq.com/v1/reverse.php?key=pk.6500b60
 var forestDataUrl = "https://api.data.gov.in/resource/4b573150-4b0e-4a38-9f4b-ae643de88f09?api-key=579b464db66ec23bdd00000157bc862d9f2146d84b764d388c4b7319&format=json&filters[states_uts]="
 var airDataUrl = "https://api.weatherbit.io/v2.0/current/airquality?key=fe3cc9eeea474df0af9999424550bdee&"
 
-var firebaseConfig = {
-    apiKey: "AIzaSyACeuNB9T7qDx_eIHe9ee_MbSYsknzws-c",
-    authDomain: "forest-59f3b.firebaseapp.com",
-    databaseURL: "https://forest-59f3b-default-rtdb.firebaseio.com",
-    projectId: "forest-59f3b",
-    storageBucket: "forest-59f3b.appspot.com",
-    messagingSenderId: "960548557802",
-    appId: "1:960548557802:web:00a6baa2c1eb91732c5893",
-    measurementId: "G-3DSWPDVCZ1"
-}
-
-
 // use the express-static middleware
 app.use(express.static("public"))
+const server = http.Server(app);
 
 // define the first route
 app.get("/newuser", function (req, res) {
@@ -214,5 +204,5 @@ function initiateParametes(callback){
 
 
 // start the server listening for requests
-app.listen(process.env.PORT || 3000, 
+server.listen(process.env.PORT || 8080, 
 	() => console.log("Server is running..."));
