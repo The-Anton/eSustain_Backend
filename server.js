8ed9cf5a38ff96b3f7ccba0c3054f7a45472c303
The-Anton <shekhar59324@gmail.com>
2021-03-15 10:02:52 +0530

    changed endpoint format15

--- server.js
+++ server.js
@@ -208,7 +208,20 @@ function initiateParametes(callback){
 // })
 // }
 
+app.get("/system/reboot", (req, res)=>{
+  setTimeout(function () {
+      // When NodeJS exits
+      process.on("exit", function () {
 
+          require("child_process").spawn(process.argv.shift(), process.argv, {
+              cwd: process.cwd(),
+              detached : true,
+              stdio: "inherit"
+          });
+      });
+      process.exit();
+  }, 1000);
+})
 // start the server listening for requests
 server.listen(process.env.PORT || 8080, 
 	() => console.log("Server is running..."));
